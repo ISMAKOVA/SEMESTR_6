ad06078b1ac480e2eee885d1051b6cac42480c3a
ISMAKOVA <ismakova.dayana@mail.ru>
2021-04-06 01:04:03 +0900

    laba 3 done

--- Artificial intelligence systems & technologies/lab_3-4/STII_lab_3.py
+++ Artificial intelligence systems & technologies/lab_3-4/STII_lab_3.py
@@ -2,6 +2,89 @@ import sys
 import qdarkgraystyle
 from PyQt5.QtGui import QIntValidator, QColor, QPainter
 from PyQt5.QtWidgets import *
+import math
+import pandas as pd
+
+
+def get_result(r, g, b):
+    brightness = get_brightness_from(r, g, b)
+    dictionary = {get_dark(brightness): "Темный", get_middle(brightness): "Средний", get_light(brightness): "Светлый"}
+    for i in sorted(dictionary.keys()):
+        print(i, dictionary[i])
+    return dictionary[sorted(dictionary.keys())[-1]], sorted(dictionary.keys())[-1]
+
+
+def get_light(x):
+    df = pd.read_csv('results2.csv', sep=',')
+    a = param_a(df, 'light')
+    k = param_k(df, 'light')
+    return 1 / (1 + math.pow(math.e, (-k * (x - a))))
+
+
+def get_dark(x):
+    df = pd.read_csv('results2.csv', sep=',')
+    a = param_a(df, 'dark')
+    k = param_k(df, 'dark')
+    return 1 / (1 + math.pow(math.e, (-k * (x - a))))
+
+
+def get_middle(x):
+    df = pd.read_csv('results2.csv', sep=',')
+    a = param_a(df, 'middle')
+    k = param_k(df, 'middle')
+    return math.pow(math.e, (-k * abs(x - a)))
+
+
+def param_a(df, column):
+    a_min = 101
+    a_max = -1
+    i = 0
+    for aVar in df[column]:
+        count = df.iloc[i, df.columns.get_loc("dark")]+df.iloc[i, df.columns.get_loc("middle")]+df.iloc[i, df.columns.get_loc("light")]
+        if round(aVar / count, 1) == 0.5:
+            if i > a_max:
+                a_max = i
+            if i < a_min:
+                a_min = i
+        i += 1
+    return round((a_max + a_min) / 2)
+
+
+def param_k(df, column):
+    a = param_a(df, column)
+    var = get_check_var(df, column)
+    count = df.iloc[var, df.columns.get_loc("dark")]+df.iloc[var, df.columns.get_loc("middle")] + \
+            df.iloc[var, df.columns.get_loc("light")]
+    v = df.iloc[var, df.columns.get_loc(column)]
+    return ((-math.log(v / count, math.e)) / abs(var - a)) if column == 'middle' else \
+        (-math.log(1 / (v / count) - 1, math.e)) / (var - a)
+
+
+def get_check_var(df, column):
+    i = 0
+    a = param_a(df, column)
+    for a_var in df[column]:
+        if (a_var != 0 and a_var != 1 and i != a
+                and a_var != df.iloc[i, df.columns.get_loc("dark")]+df.iloc[i, df.columns.get_loc("middle")] +
+                df.iloc[i, df.columns.get_loc("light")]):
+            return i
+        i += 1
+
+
+def set_result(r, g, b, category):
+    df = pd.read_csv('results2.csv', sep=',')
+    brightness = get_brightness_from(r, g, b)
+    df.iloc[brightness, df.columns.get_loc(category)] += 1
+    df.iloc[brightness, df.columns.get_loc("number_ppl")] += 1
+    df.to_csv('results2.csv', index=False, header=True)
+
+
+def get_brightness_from(r, g, b):
+    r = int(r)/2.55
+    g = int(g)/2.55
+    b = int(b) / 2.55
+    brightness = 0.2126*r+0.7152*g+0.0722*b
+    return round(brightness)
 
 
 class AppWindow(QMainWindow):
@@ -62,7 +145,7 @@ class TabForExpert(QWidget):
         self.button = QPushButton('Задать цвет', self)
         self.button_save = QPushButton('Сохранить', self)
         self.combo = QComboBox(self)
-        self.combo.addItems(["Темный", "Средний", "Светлый"])
+        self.combo.addItems(["dark", "middle", "light"])
         self.layout = QGridLayout(self)
         self.layout.addWidget(self.R, 0, 0)
         self.layout.addWidget(self.G, 0, 1)
@@ -73,22 +156,25 @@ class TabForExpert(QWidget):
         self.s.setBackgroundBrush(QColor(0, 0, 0))
         self.g = QGraphicsView(self.s)
         self.g.render(QPainter())
-
+        self.label = QLabel(self)
         self.layout.addWidget(self.g, 1, 0, 1, 4)
         self.layout.addWidget(self.combo, 2, 0)
         self.layout.addWidget(self.button_save, 2, 1)
+        self.layout.addWidget(self.label, 2, 2)
         self.setLayout(self.layout)
 
         self.button.clicked.connect(self.btn_clicked)
+        self.button_save.clicked.connect(self.write_dataset)
 
     def btn_clicked(self):
         self.s.setBackgroundBrush(QColor(int(self.R.text()), int(self.G.text()), int(self.B.text())))
         self.g.render(QPainter())
-        # text = self.comboBox.currentText()
 
     def write_dataset(self):
-
-        return ""
+        brightness = get_brightness_from(int(self.R.text()), int(self.G.text()), int(self.B.text()))
+        self.label.setText("Яркость: "+str(brightness))
+        category = self.combo.currentText()
+        set_result(int(self.R.text()), int(self.G.text()), int(self.B.text()), category)
 
 
 class TabForAnalysis(QWidget):
@@ -111,14 +197,29 @@ class TabForAnalysis(QWidget):
 
         self.button = QPushButton('Определить цвет', self)
         self.label = QLabel(self)
+
+        self.s = QGraphicsScene()
+        self.s.setBackgroundBrush(QColor(0, 0, 0))
+        self.g = QGraphicsView(self.s)
+        self.g.render(QPainter())
+
         self.layout = QGridLayout(self)
         self.layout.addWidget(self.R, 0, 0)
         self.layout.addWidget(self.G, 0, 1)
         self.layout.addWidget(self.B, 0, 2)
         self.layout.addWidget(self.button, 0, 3)
-        self.layout.addWidget(self.label, 1, 0)
+        self.layout.addWidget(self.g, 1, 0, 1, 4)
+        self.layout.addWidget(self.label, 2, 0)
         self.setLayout(self.layout)
 
+        self.button.clicked.connect(self.get_result)
+
+
+    def get_result(self):
+        category, count = get_result(int(self.R.text()), int(self.G.text()), int(self.B.text()))
+        self.s.setBackgroundBrush(QColor(int(self.R.text()), int(self.G.text()), int(self.B.text())))
+        self.g.render(QPainter())
+        self.label.setText("Результат: {0} ({1})".format(category, count))
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- Artificial intelligence systems & technologies/lab_3-4/STII_lab_3_func.py
+++ Artificial intelligence systems & technologies/lab_3-4/STII_lab_3_func.py
@@ -1,22 +1,80 @@
-import csv
+import math
+import pandas as pd
 
 
-def write_read_csv(brightness, category):
-    csv_file = csv.reader(open('results2.csv', "r"), delimiter=",")
-    file_text = []
-    for row in csv_file:
-        if row[0].isdigit():
-            if int(row[0]) == brightness:
-                row[category] = int(row[category]) + 1
-                row[4] = int(row[4]) + 1
-            file_text.append(row)
+def get_result(r, g, b):
+    brightness = get_brightness_from(r, g, b)
+    dictionary = {get_dark(brightness): "Темный", get_middle(brightness): "Средний", get_light(brightness): "Светлый"}
+    res = {}
+    # for i in sorted(dictionary.keys()):
+    #     res[i] = dictionary[i]
+    #     print(res[i], dictionary[i])
+    return dictionary[sorted(dictionary.keys())[-1]], sorted(dictionary.keys())[-1]
 
-    with open("results2.csv", 'w', encoding='utf-8') as file:
-        a_pen = csv.writer(file)
-        columns = ["brightness", "dark", "middle", "light", "number_ppl"]
-        a_pen.writerow(columns)
-        for line in file_text:
-            a_pen.writerow([line[0], line[1], line[2], line[3], line[4]])
+
+def get_light(x):
+    df = pd.read_csv('results2.csv', sep=',')
+    a = param_a(df, 'light')
+    k = param_k(df, 'light')
+    return 1 / (1 + math.pow(math.e, (-k * (x - a))))
+
+
+def get_dark(x):
+    df = pd.read_csv('results2.csv', sep=',')
+    a = param_a(df, 'dark')
+    k = param_k(df, 'dark')
+    return 1 / (1 + math.pow(math.e, (-k * (x - a))))
+
+
+def get_middle(x):
+    df = pd.read_csv('results2.csv', sep=',')
+    a = param_a(df, 'middle')
+    k = param_k(df, 'middle')
+    return math.pow(math.e, (-k * abs(x - a)))
+
+
+def param_a(df, column):
+    a_min = 101
+    a_max = -1
+    i = 0
+    for aVar in df[column]:
+        count = df.iloc[i, df.columns.get_loc("dark")]+df.iloc[i, df.columns.get_loc("middle")]+df.iloc[i, df.columns.get_loc("light")]
+        if round(aVar / count, 1) == 0.5:
+            if i > a_max:
+                a_max = i
+            if i < a_min:
+                a_min = i
+        i += 1
+    return round((a_max + a_min) / 2)
+
+
+def param_k(df, column):
+    a = param_a(df, column)
+    var = get_check_var(df, column)
+    count = df.iloc[var, df.columns.get_loc("dark")]+df.iloc[var, df.columns.get_loc("middle")] + \
+            df.iloc[var, df.columns.get_loc("light")]
+    v = df.iloc[var, df.columns.get_loc(column)]
+    return ((-math.log(v / count, math.e)) / abs(var - a)) if column == 'middle' else \
+        (-math.log(1 / (v / count) - 1, math.e)) / (var - a)
+
+
+def get_check_var(df, column):
+    i = 0
+    a = param_a(df, column)
+    for a_var in df[column]:
+        if (a_var != 0 and a_var != 1 and i != a
+                and a_var != df.iloc[i, df.columns.get_loc("dark")]+df.iloc[i, df.columns.get_loc("middle")] +
+                df.iloc[i, df.columns.get_loc("light")]):
+            return i
+        i += 1
+
+
+def set_result(r, g, b, category):
+    df = pd.read_csv('results2.csv', sep=',')
+    brightness = get_brightness_from(r, g, b)
+    df.iloc[brightness, df.columns.get_loc(category)] += 1
+    df.iloc[brightness, df.columns.get_loc("number_ppl")] += 1
+    df.to_csv('results2.csv', index=False, header=True)
 
 
 def get_brightness_from(r, g, b):
@@ -24,17 +82,9 @@ def get_brightness_from(r, g, b):
     g = int(g)/2.55
     b = int(b) / 2.55
     brightness = 0.2126*r+0.7152*g+0.0722*b
-    print(round(brightness))
-    with open("results2.csv", "a") as file:
-        a_pen = csv.writer(file)
-        for i in range(100):
-            a_pen.writerow([str(i), 0, 0, 0, 0])
-
-    return ""
+    return round(brightness)
 
 
-# get_brightness_from(230, 10, 255)
-
-write_read_csv(3, 2)
-
-#category: brightness = 0,dark = 1,middle = 2,light = 3,number_ppl = 4
+dfd = pd.read_csv('results2.csv', sep=',')
+# print(param_k(dfd, "light"))
+print(get_result(25, 25, 20))
