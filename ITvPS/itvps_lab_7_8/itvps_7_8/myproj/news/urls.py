0fca1f23b8e1e8bb15a895b46c928c76e0d98c83
ISMAKOVA <48486359+ismakova@users.noreply.github.com>
2021-05-07 20:52:39 +0900

    итвпс лаба 7-8

--- ITvPS/itvps_lab_7_8/itvps_7_8/myproj/news/urls.py
+++ ITvPS/itvps_lab_7_8/itvps_7_8/myproj/news/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
-
+    path('', views.article),
+    path('index', views.index),
+    path('rubrika', views.rubrika)
 ]
